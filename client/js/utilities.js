498692dc5f18c81f3505e2ba6ffb304e8908ce92
NixxdeOfficial <64833558+NixxdeOfficial@users.noreply.github.com>
2021-11-15 21:58:45 +0100

    customization code decoder and style setter, need fixes

--- client/js/utilities.js
+++ client/js/utilities.js
@@ -2,7 +2,7 @@ function parseUrls(text) {
 	var words = text.split(" ");
 
 	words.forEach(function (item, index) {
-		if(item.startsWith("https://") || item.startsWith("http://")) {
+		if (item.startsWith("https://") || item.startsWith("http://")) {
 			words[index] = `<a href="${item}" class="msgUrl">${item}</a>`;
 		}
 	});
@@ -13,8 +13,8 @@ function parseUrls(text) {
 /**
  * Returns a random number between min (inclusive) and max (exclusive)
  */
- function getRandomArbitrary(min, max) {
-    return Math.random() * (max - min) + min;
+function getRandomArbitrary(min, max) {
+	return Math.random() * (max - min) + min;
 }
 
 /**
@@ -25,21 +25,21 @@ function parseUrls(text) {
  * Using Math.round() will give you a non-uniform distribution!
  */
 function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function uuidv4() {
-    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-    );
-  }
+	return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+		(c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+	);
+}
 
-  function setCookie(cname, cvalue) {
+function setCookie(cname, cvalue) {
 	const d = new Date();
-	d.setTime(d.getTime() + (365*24*60*60*1000));
-	let expires = "expires="+ d.toUTCString();
+	d.setTime(d.getTime() + (365 * 24 * 60 * 60 * 1000));
+	let expires = "expires=" + d.toUTCString();
 	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
@@ -47,7 +47,7 @@ function getCookie(cname) {
 	let name = cname + "=";
 	let decodedCookie = decodeURIComponent(document.cookie);
 	let ca = decodedCookie.split(';');
-	for(let i = 0; i <ca.length; i++) {
+	for (let i = 0; i < ca.length; i++) {
 		let c = ca[i];
 		while (c.charAt(0) == ' ') {
 			c = c.substring(1);
@@ -57,4 +57,59 @@ function getCookie(cname) {
 		}
 	}
 	return "";
+}
+
+function decodeSaveCustomizationCode(code = String, setCustomization = false, debug = false) {
+	// cbw-30;sbh-15;bg-"BASE64ENCODEDURL";fs-11;ff-"BASE64ENCODEDFONTNAME";cbp-[X,Y,W,H]
+
+	let cells = code.split(";");
+	if (debug) console.log("length: ", cells.length);
+	let jsonThing = "";
+
+	cells.forEach((value, index) => {
+		let AAAAA = value.split("-");
+		if (debug) {
+			console.log(index, value);
+			console.log(index, `"${AAAAA[0]}":${AAAAA[1]},`)
+		}
+		if (index === cells.length - 1) {
+			jsonThing = jsonThing + `"${AAAAA[0]}":${AAAAA[1]}`;
+		} else {
+			jsonThing = jsonThing + `"${AAAAA[0]}":${AAAAA[1]},`;
+		}
+	})
+
+	if (debug) console.log('{' + jsonThing + '}');
+	jsonThing = "{" + jsonThing + "}";
+	let parsed = JSON.parse(jsonThing);
+	if (!setCustomization) {
+		return parsed;
+	} else {
+		if(parsed.cbw && parsed.cbw > 5 && parsed.cbw < 50) { // WIDTH IS IN PERCENT NOT IN PIXELS!!!
+			$("#channels").css("width", `${parsed.cbw}%`)
+		} else {
+			throw Error("Invalid Channels Bar Width")
+		}
+
+		if(parsed.sbh && parsed.sbh > 3 && parsed.sbh < 25) { // its height, not width.
+			$("#servers").css("height", `${parsed.sbh}%`)
+		} else {
+			throw Error("Invalid Servers Bar Height")
+		}
+
+		if(parsed.bg) {
+			$("body").css("background", `url(${atob(parsed.bg)})`)
+		} else {
+			throw Error("Background URL is undefined.")
+		}
+
+		if(parsed.fs && parsed.fs > 3 && parsed.fs < 25) { // font size
+			$("body").css("font-size", `${parsed.fs}px`)
+		} else {
+			throw Error("Invalid Font Size")
+		}
+
+
+	}
+
 }
