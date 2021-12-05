21f651f0349c4b0d5a47ed95721458b6ccfac248
SomeEver <roomberapp@gmail.com>
2021-12-05 16:58:49 +0300

    heroku prep part 2

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    dbUrl: 'mongodb+srv://roomber:6AhiVVV4sYoRTBLk@roomber-db.lhfwu.mongodb.net/roomber?retryWrites=true&w=majority',
+    ngrokAuthtoken: '1ph9ua1UFi9fxzZfatQUjiSZ2rt_3oo7p7hAhCxGMfZXoUETc',
+    enableNgrok: true,
+    openNgrokURL: false
+};
