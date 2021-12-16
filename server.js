b11ac4eba339afea1014cbd01cf2e40ae8ab48f1
NixxdeOfficial <64833558+NixxdeOfficial@users.noreply.github.com>
2021-12-16 20:52:34 +0100

    server - sendVerifyEmail() function

--- server.js
+++ server.js
@@ -10,6 +10,15 @@ var chalk = require('chalk');
 const ngrok = require('ngrok');
 const open = require('open');
 const ogs = require('open-graph-scraper');
+const nodemailer = require('nodemailer');
+const transporter = nodemailer.createTransport({
+	service: 'gmail',
+	auth: {
+	user: 'verify.roomber@gmail.com',
+	pass: config.notfunny
+	}
+	});
+
 let maintenance = false;
 
 const enableNgrok = config.enableNgrok;
@@ -474,6 +483,48 @@ app.post('/createChannel', (req, res) => {
 		}
 	})
 })
+
+/*app.post('/testsend', (req, res) => {
+
+	sendVerifyEmail(req.body, (err,code) => {
+		if(err) {
+			res.send(err);
+		} else {
+			res.send(code);
+		}
+	})
+})*/
+
+/**
+ * 
+ * sends a 2FA email, callback includes error and "verifimient" code [callback(err,code)]
+ * 
+ * options is like
+ * 
+ * {
+ * 		username: "username",
+ * 		address: "someone@romb.lol"
+ * }
+ * 
+ */
+function sendVerifyEmail(options, callback) {
+	const code = Math.floor(100000 + Math.random() * 900000).toString();
+	var msghtml = "<center><img src='http://roomber-dev.herokuapp.com/assets/roomberfull2.png' alt='Roomber' style='font-size: 2.5rem;color:black' height='200'></center><div style='border:4px solid #a7a7a7;border-radius:4px;width:80%;height:40%;background:#fff;width:50%;margin:0 auto;padding:10px;'><h1 style='color: black;'>Hi <p style='font-weight:700;padding:2px 4px;border-radius:4px;margin:0 5px 0 0;display:inline-block;background-color:rgba(0,0,0,.2)'>{{username}}</p>, welcome to Roomber!</h1><br>Thanks for creating a Roomber account! Unfortunately you have to confirm your email address before you start. But that's no problem, just Ctrl+C & Ctrl+V this code to finish everything up!<br><p style='text-align:center;color:#000;font-size:4rem;font-family:Arial,Helvetica,sans-serif'>{{vcode}}</p></div>"
+	const mailOptions = {
+		from: 'Roomber Verification',
+		to: options.address,
+		subject: 'Verify your Roomber account',
+		html: msghtml.replace(/{{username}}/g, options.username).replace(/{{vcode}}/g, code)
+	};
+	transporter.sendMail(mailOptions, (err, info) => {
+		console.log(err, info);
+		if(err) {
+			callback(err);
+		} else {
+			callback(err, code);
+		}
+	})
+}
 
 app.post('/userid', (req, res) => {
 	User.find(req.body, (err, user) => {
