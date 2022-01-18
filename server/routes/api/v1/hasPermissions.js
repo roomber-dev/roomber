5cd8029d1e3f9d27e4ae550a48587408dbee63e3
somenever <95498239+somenever@users.noreply.github.com>
2022-01-18 22:17:56 +0300

    Update hasPermissions.js

--- server/routes/api/v1/hasPermissions.js
+++ server/routes/api/v1/hasPermissions.js
@@ -1,7 +1,7 @@
 module.exports = require('express').Router({ mergeParams: true })
     .post('/v1/hasPermissions', (req, res) => {
         req.db.User.find({ _id: req.body.user }, (err, [user]) => {
-            user.hasPermissions(req.db, req.body["permissions[]"], result => {
+            user.hasPermissions(req.db, req.body.permissions, result => {
                 res.send(result);
             })
         })
