08a340c602deaced0f31995b83b061afd184609e
somenever <95498239+somenever@users.noreply.github.com>
2022-01-23 22:10:23 +0300

    Fix attachments

--- client/js/client.js
+++ client/js/client.js
@@ -577,9 +577,10 @@ function composeMessageContent(message, messageText) {
 		createEmbed(message.parent().parent().parent().parent().parent().prop("id"), url, "en-GB");
 	});
 	const attachment = message.parent().parent().parent().find(".attachment");
-	if(attachment[0] && attachment[0].naturalWidth == 0) {
-		attachment.parent().html(`<div class="attachment" style="padding: 4px; border: 1px solid rgba(0,0,0,0.1);"><i class="megasmall material-icons">description</i>test.txt</div>`);
-	}
+        const s = attachment.attr("src").split("/");
+	attachment.on("error", function() {
+		attachment.parent().html(`<div class="attachment" style="padding: 4px; border: 1px solid rgba(0,0,0,0.1);"><i class="megasmall material-icons">description</i>${s[s.length - 1]}</div>`);
+	});
 }
 
 function getChats() {
